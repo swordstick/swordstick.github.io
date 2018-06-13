254a62f322ef79e01d50f77b7d63f54b4f926c48
robinwen <dbarobinwen@gmail.com>
2018-06-13 22:12:54 +0800

    [README] Update README

--- _backup/scripts/add_article.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Robin Wen
-# Date: 2015-1-10 15:47:08
-# Desc: Auto push after add article.
-# Update: Update Commit info at 2015-03-04 17:16:28
-
-git add -A .
-git commit -m "[Post] Add article."
-git push origin master
--- _backup/theme/f_guestbook.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: GuestBook
-comments: no
-permalink: /guestbook/
----
-
-Hi, I am Robin. I removed disqus comments, please contact me via Gmail: dbarobinwen@gmail.com
